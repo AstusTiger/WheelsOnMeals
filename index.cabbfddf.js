da851e1cd5175f6be85dcdc33c39b7a77c2a5034
AstusTiger <107391458+AstusTiger@users.noreply.github.com>
2022-08-29 15:37:47 +0000

    Deploying to gh-pages from @ AstusTiger/WheelsOnMeals@9a1837605245f7c964363a376e76e37e4c60458d 🚀

---
+++ index.cabbfddf.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("[data-body]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.cabbfddf.js.map
