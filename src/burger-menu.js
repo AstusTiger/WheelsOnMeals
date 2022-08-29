ab3900ffb588c67779029dfa00cccd72c9bf3e2b
Pavlo Radchenko <Pavlo.radchenko93@gmail.com>
2022-08-29 18:33:01 +0300

    update

--- src/burger-menu.js
+++ src/burger-menu.js
@@ -15,19 +15,3 @@
     mobileLogoRef.classList.toggle('logo--hidden');
   });
 })();
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-menu-button]'),
-//     closeModalBtn: document.querySelector('[data-menu-button-close]'),
-//     modal: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('modal__backdrop--is-hidden');
-//   }
-// })();
-
